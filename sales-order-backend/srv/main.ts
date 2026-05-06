60e1d8424759889516ea2cf1e3de7e4daaa8b21b
Gustavo Martos <gustavo.martos@numenit.com>
2026-05-06 11:50:32 -0300

    feat: add tsconfig.js

---
+++ sales-order-backend/srv/main.ts
@@ -0,0 +1,8 @@
+import cds from '@sap/cds';
+
+export default class MainService extends cds.ApplicationService {
+    async init() {
+        // A mágica vai acontecer aqui depois
+        return super.init();
+    }
+}
